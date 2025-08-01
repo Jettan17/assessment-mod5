1c8c822e0328ea68c54f70a771761b7d581fe49b
Jettan17 <jethrotan2006@gmail.com>
2025-08-01 15:52:21 +0800

    Changed text from Hello World to Hello instructors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: "Hello world"});
+  res.send({ message: "Hello instructors"});
 });
 
 module.exports = app;
